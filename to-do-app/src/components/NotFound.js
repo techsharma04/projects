e972c961df5db15d87a6a876c3405301e6d87876
techsharma04 <techsharma04@outlook.com>
2023-10-15 22:52:36 -0400

    to-do-app removed

--- to-do-app/src/components/NotFound.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const NotFound = () => {
-  return (
-    <p className='not-found-message'>Page you're looking for doesn't exist</p>
-  )
-}
-
-export default NotFound
--- to-do-app/src/store.js
+++
@@ -1,6 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit'
-import tasksSlice from './reducers/tasksSlice';
-
-export const store = configureStore({
-  reducer: { tasks: tasksSlice },
-})
